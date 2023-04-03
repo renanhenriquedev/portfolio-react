83327639468cb7103cd80f30a404b3acefe8a633
Renan <renanhenrique20222@gmail.com>
2023-04-03 19:12:01 -0300

    finished page project with fix your modularization

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import styles from './Projects.module.css'
-import pixelArt from '../../imagens./pixelart.png'
+import pixelArt from '../../imagens/pixel-art.png'
+import list from '../../imagens/list.png'
+import burguer from '../../imagens/burguer.png'
+
+
 
 function Projects() {
     return (
         <main>
             <div id={styles.container}>
 
-                <img id={`${styles.pixel} ${styles.img}`} src={pixelArt}/>
+                <img id={styles.pixel} className={styles.img}src={pixelArt}/>
 
                     <aside className={styles.aside}>Projeto com funcionalidades de HTML, CSS e Javascript com o objetivo de simular uma plataforma de
                         pixel art para fazer os seus desenhos.</aside>
 
                     <div id={styles.funcionality}>
                         <a href="https://github.com/renanhenriquedev/Pixel-Art">
-                            <button type="button" id={styles.git} className={styles.button-28}>Github</button>
+                            <button type="button" id={styles.git} className={styles['button-28']}>Github</button>
                         </a>
 
                         <a href="https://pixelart-project.surge.sh/">
-                            <button type="button" id={styles.git} className={styles.button-28}>Deploy</button>
+                            <button type="button" id={styles.git} className={styles['button-28']}>Deploy</button>
                         </a>
                     </div>
 
@@ -26,7 +30,7 @@ function Projects() {
 
             <div id={styles.container}>
 
-                <img id={`${styles.pixel} ${styles.img}`} src="./imagens/list.png" width="10px"/>
+                <img id={styles.pixel} className={styles.img}src={list}/>
 
                     <aside className={styles.aside}>Projeto com funcionalidades de HTML, CSS e Javascript com o objetivo de simular uma plataforma de
                         lista de tarefas para o dia a dia.</aside>
@@ -34,11 +38,11 @@ function Projects() {
                     <div id={styles.funcionality}>
 
                         <a href="https://github.com/renanhenriquedev/to-do-list">
-                            <button type="button" id={styles.git} className={styles.button-28}>Github</button>
+                            <button type="button" id={styles.git} className={styles['button-28']}>Github</button>
                         </a>
 
                         <a href="https://list-project.surge.sh/">
-                            <button type="button" id={styles.git} className={styles.button-28}>Deploy</button>
+                            <button type="button" id={styles.git} className={styles['button-28']}>Deploy</button>
                         </a>
 
                     </div>
@@ -46,7 +50,7 @@ function Projects() {
 
             <div id={styles.container}>
 
-                <img id={`${styles.pixel} ${styles.img}`} src="./imagens/burguer.png" width="10px"/>
+                <img id={styles.pixel} className={styles.img}src={burguer}/>
 
                     <aside className={styles.aside}>Projeto com funcionalidades de HTML e CSS com o objeito de simular um site de uma hamburgueria local.
                     </aside>
@@ -54,11 +58,11 @@ function Projects() {
                     <div id={styles.funcionality}>
 
                         <a href="https://github.com/renanhenriquedev/hamburger-shop">
-                            <button type="button" id={styles.git} className={styles.button-28}>Github</button>
+                            <button type="button" id={styles.git} className={styles['button-28']}>Github</button>
                         </a>
 
                         <a href="https://burguer-project.surge.sh/">
-                            <button type="button" id={styles.git} className={styles.button-28}>Deploy</button>
+                            <button type="button" id={styles.git} className={styles['button-28']}>Deploy</button>
                         </a>
 
                     </div>
